dbebb327e32798357cfa2c2685f70b84c8602b1c
rpradosilva <contato@rprado.design>
2020-08-01 20:56:30 -0300

    fix: dark mode; Add pages: about and recipes

---
+++ public/scripts/theme.js
@@ -0,0 +1,31 @@
+const changeThemeBtn = document.querySelector(".navbar-theme")
+const bodyDocument = document.getElementsByTagName("BODY")[0]
+
+
+function storageTheme(id, value) {
+    localStorage.setItem(id, JSON.stringify(value))
+}
+
+function changeTheme() {
+    const currentTheme = bodyDocument.getAttribute("class")
+    if (currentTheme == null || currentTheme == "light" || currentTheme == "") {
+        bodyDocument.classList.add("dark")
+        storageTheme("theme", "dark")
+    } else {
+        bodyDocument.classList.remove("dark")
+        storageTheme("theme", "light")
+    }
+}
+
+function loadTheme() {
+    const defaultTheme = JSON.parse(localStorage.getItem("theme"))
+    if (defaultTheme == null || defaultTheme == "light" || defaultTheme == "") {
+        bodyDocument.classList.remove("dark")
+    } else {
+        bodyDocument.classList.add("dark")
+    }
+}
+
+changeThemeBtn.addEventListener("click", changeTheme)
+
+loadTheme()
